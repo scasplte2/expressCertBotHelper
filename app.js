ca495a2158203cbda23ac6384ee4ffe8211915d4
James Aman <jim.a.aman@gmail.com>
2020-02-07 15:26:29 -0600

    update launch to work with pm2

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const config = require('config');
 // Configure & Run the http server
 const app = express();
 
+console.log(process.env.NODE_ENV)
+console.log(process.env.PORT)
+console.log(process.env.NODE_APP_INSTANCE)
+
+// set default port
+PORT = process.env.PORT || 80
+
 app.get("/.well-known/acme-challenge/" + config.get("challenge"),
         (req, res) => res.send(config.get("response")));
 
-app.listen(80, () => {
-  console.log('HTTP server running on port 80');
+app.listen(PORT, () => {
+  console.log('certbotHelper - HTTP server running on port ' + PORT);
 });
