b2747397a167e25b71305ee1db9d7aa491a8fc2f
James Aman <jim.a.aman@gmail.com>
2020-02-10 14:43:38 -0600

    look for env variables

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // Dependencies
 const express = require('express');
-const config = require('config');
+
+// set the process title so we can stop it
+process.title = "ecbHelper"
 
 // Configure & Run the http server
 const app = express();
 
 // Check if there are env variables
-const challenge = config.has("certbot.challenge") ? config.get("certbot.challenge") : "url-string"
-const response = config.has("certbot.response") ? config.get("certbot.response") : "It works!"
+const challenge = process.env.CERTBOT_TOKEN || "url-string"
+const response = process.env.CERTBOT_VALIDATION || "It works!"
 
 // set default port
 PORT = process.env.PORT || 80
 
 app.get("/.well-known/acme-challenge/" + challenge, (req, res) => res.send(response));
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('certbotHelper - HTTP server running on port ' + PORT);
 });
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Process terminated')
+  })
+})
